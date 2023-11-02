af0a14e95abad535941bd2965e1cf80aeeb09fd9
BhavleenKaurSethi <97071574+BhavleenKaurSethi@users.noreply.github.com>
2023-11-02 14:35:30 +1100

    Version 2 of app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import mysql.connector
 
 app = FastAPI()
 
-db_connection = mysql.connector.connect(user='root', password=password,
+db_connection = mysql.connector.connect(user='root', password='Harjas1703.',
                               host='127.0.0.1',
                               database='trading_platform')
 
 @app.get("/query/{option}")
-async def read_item(option: int):
+async def read_item(option: int, share_id: int | None = None,
+                    broker_id: int | None = None,
+                    date_range_min: str | None = None,
+                    date_range_max: str | None = None):
    
     if option == 1:
         return get_brokers_list(db_connection)
@@ -17,5 +20,7 @@ async def read_item(option: int):
         return get_shares_list(db_connection)
     if option == 3:
         return get_trade_list(db_connection)
+    if option == 4:
+        return search_trade(db_connection, share_id, broker_id, date_range_min, date_range_max)  
     return 'Invalid option'
 
